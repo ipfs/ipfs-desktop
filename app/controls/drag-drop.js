db1ac7ac8e17b3233a6357e60c5f40f623152070
David Dias <daviddias.p@gmail.com>
2015-08-09 10:35:12 +0200

    list the files works, now just have to make it pretty

--- app/controls/drag-drop.js
+++ app/controls/drag-drop.js
@@ -1,9 +1,14 @@
 var init = require('./../init')
 var ipfsAPI = require('ipfs-api')('localhost', '5001')
+var clipboard = require('clipboard')
+var path = require('path')
 
-// var ipc = require('ipc')
+var ipc = require('ipc')
 
 exports = module.exports = dragDrop
+
+// TODO persist this to disk
+var filesUploaded = []
 
 function dragDrop (event, fileArray) {
   if (!init.getIPFS()) {
@@ -11,11 +16,13 @@ function dragDrop (event, fileArray) {
     return console.error(new Error('Can\'t upload file, IPFS Node is off'))
   }
 
-  console.log('uploading ->', fileArray)
-
   if (fileArray.length > 1) {
     return console.log('Only supports single files for now')
   }
+
+  console.log('uploading ->', path.basename(fileArray[0]))
+
+  ipc.emit('uploading', {Name: path.basename(fileArray[0])})
 
   ipfsAPI.add(fileArray, function (err, res) {
     if (err || !res) {
@@ -23,6 +30,10 @@ function dragDrop (event, fileArray) {
     }
 
     res.forEach(function (file) {
+      clipboard.writeText('http://gateway.ipfs.io/ipfs/' + file.Hash)
+      filesUploaded.push(file)
+      ipc.emit('uploaded', file)
+
       console.log(file.Hash)
       console.log(file.Name)
     })
--- app/js/menubar.jsx
+++ app/js/menubar.jsx
@@ -20,6 +20,7 @@ var Menu = React.createClass({
 
   componentDidMount: function () {
     var self = this
+    self.state.files = []
 
     // -- Listen to control events
 
@@ -33,6 +34,26 @@ var Menu = React.createClass({
 
     ipc.on('stats', function (stats) {
       self.setState({stats: stats})
+    })
+
+    ipc.on('uploading', function (file) {
+      console.log('file being uploaded: ' + file.Name)
+      if (self.state.files.length >= 5) {
+        self.state.files.shift()
+      }
+      file.uploaded = false
+      self.state.files.push(file)
+      self.setState({files: self.state.files})
+    })
+
+    ipc.on('uploaded', function (file) {
+      for (var i = 0; i < self.state.files.length; i++) {
+        if (self.state.files[i].Name === file.Name) {
+          self.state.files[i].Hash = file.Hash
+          self.state.files[i].uploaded = true
+        }
+      }
+      self.setState({files: self.state.files})
     })
 
     ipc.emit('request-state')
@@ -71,21 +92,16 @@ var Menu = React.createClass({
   render: function () {
     var self = this
 
-    var version = this.state.version ? (
-      <div className='row'>
-        <div className='panel panel-default version'>
-          {this.state.version}
-        </div>
-      </div>) : null
+    var image = (this.state.status !== 'running'
+      ? '../../node_modules/ipfs-logo/ipfs-logo-128-black.png'
+      : '../../node_modules/ipfs-logo/ipfs-logo-128-ice.png')
+
+    var status = <div className='row status'>{ self.state.status }</div>
 
     var toggles = [
       (self.state.status !== 'uninitialized') &&
       <Toggle label='IPFS Node' toggle={self.toggleDaemon}/>
     ]
-
-    var image = (this.state.status !== 'running'
-      ? '../../node_modules/ipfs-logo/ipfs-logo-128-black.png'
-      : '../../node_modules/ipfs-logo/ipfs-logo-128-ice.png')
 
     // var uninitialized = (this.state.status === 'uninitialized')
 
@@ -100,7 +116,7 @@ var Menu = React.createClass({
           <a href='#'
             className='list-group-item'
             onClick={self.openBrowser}>
-            Open in browser
+            Open in Browser
           </a>
         </div>
       </div>
@@ -124,7 +140,20 @@ var Menu = React.createClass({
       </div>
     ) : null
 
-    var status = <div className='row status'>{ self.state.status }</div>
+    var version = this.state.version ? (
+      <div className='row'>
+        <div className='panel panel-default version'>
+          {this.state.version}
+        </div>
+      </div>) : null
+
+    var files = this.state.files && this.state.files.length > 0 ? (
+      <div className='row'>
+        {(_.map(self.state.files, function (value, name) {
+          return (<p>{name} {value}</p>)
+        }))}
+      </div>
+    ) : null
 
     return (
       <div className='padding'>
@@ -139,6 +168,7 @@ var Menu = React.createClass({
           { open }
           { stats }
           { version }
+          { files }
         </div>
       </div>
     )
