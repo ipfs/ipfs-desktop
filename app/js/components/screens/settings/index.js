63aa7340b4571975cb3465c0bbf04522a4b3748c
dignifiedquire <dignifiedquire@gmail.com>
2015-10-11 21:28:10 +0200

    Cleanup settings

---
+++ app/js/components/screens/settings/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class Settings extends Component {
+  render () {
+    return (
+      <div>
+        Settings
+      </div>
+    )
+  }
+}
--- app/js/settings.js
+++ app/js/settings.js
@@ -1,39 +1,8 @@
-var React = require('react/addons')
+import React from 'react'
+import {render} from 'react-dom'
 
-var Settings = React.createClass({
-
-  // -- Initialize Component
-
-  getInitialState: function () {
-    return {}
-  },
-
-  componentDidMount: function () {
-    // var self = this
-  },
-
-  // -- Actions
-
-  // -- Render
-
-  render: function () {
-    // var self = this
-
-    return (
-      <div className='col-xs-9 col-xs-offset-2'>
-        <div className='panel panel-default '>
-          <div className='panel-heading'>
-            Settings
-          </div>
-          <div className='panel-body'>
-            Yellow
-          </div>
-        </div>
-      </div>
-    )
-  }
-})
+import SettingsScreen from './components/screens/settings/index'
 
 document.addEventListener('DOMContentLoaded', () => {
-  React.render(<Settings />, document.getElementById('settings'))
+  render(<SettingsScreen />, document.getElementById('settings'))
 })
