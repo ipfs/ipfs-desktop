cce74cbac4fa898a048b98a984cb197ce60ff8dd
Henrique Dias <hacdias@gmail.com>
2017-12-04 19:00:58 +0000

    remove unuses import

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {uploadFiles} from './controls/upload-files'
 import {join} from 'path'
 import {lookupPretty} from 'ipfs-geoip'
 import config, {logger} from './config'
-import {dialog, ipcMain, Menu, shell, app} from 'electron'
+import {dialog, ipcMain, shell, app} from 'electron'
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
