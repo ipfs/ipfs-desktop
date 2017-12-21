9c54415ad2cacae1d6435da6b79f8361f5af10a1
Henrique Dias <hacdias@gmail.com>
2017-12-21 14:41:59 +0000

    panes

--- src/js/components/view/pane-container.js
+++ src/js/components/view/pane-container.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+/**
+ * Is a Pane.
+ *
+ * @param {Object} props
+ *
+ * @prop {Node} children must be elements of component Pane
+ *
+ * @return {ReactElement}
+ */
 export default function PaneContainer (props) {
   return (
     <div className='panes'>
--- src/js/components/view/pane.js
+++ src/js/components/view/pane.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+/**
+ * Is a Pane.
+ *
+ * @param {Object} props
+ *
+ * @prop {Node} children
+ * @prop {String} class are additional class names
+ *
+ * @return {ReactElement}
+ */
 export default function Pane (props) {
   let className = 'pane'
   if (props.class) {
