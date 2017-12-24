836ce5a3962f1e5cae7d90f37946c62e036dddb7
Henrique Dias <hacdias@gmail.com>
2017-12-24 13:37:37 +0000

    refactor: use eventlistener on key/value store

--- src/utils/key-value-store.js
+++ src/utils/key-value-store.js
@@ -3,7 +3,6 @@ import FileStore from './file-store'
 export default class KeyValueStore extends FileStore {
   constructor (location) {
     super(location, {})
-    this.listeners = {}
   }
 
   get (key) {
@@ -15,20 +14,8 @@ export default class KeyValueStore extends FileStore {
       this.listeners[key] = []
     }
 
-    const oldValue = this.data[key]
+    this.emit(key, this.data[key], value)
     this.data[key] = value
     this.write()
-
-    this.listeners[key].forEach(listener => {
-      listener(oldValue, value)
-    })
-  }
-
-  listen (key, listener) {
-    if (!this.listeners[key]) {
-      this.listeners[key] = []
-    }
-
-    this.listeners[key].push(listener)
   }
 }
