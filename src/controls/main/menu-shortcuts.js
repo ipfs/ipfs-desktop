0cd6019bec31f96d12ebf7e3458784f47f3c0e9d
Henrique Dias <hacdias@gmail.com>
2018-01-14 11:52:30 +0000

    feat: dev tools on normal mode (#594)

---
+++ src/controls/main/menu-shortcuts.js
@@ -0,0 +1,29 @@
+import {Menu} from 'electron'
+
+export default function (opts) {
+  Menu.setApplicationMenu(Menu.buildFromTemplate([
+    {
+      // Edit menu entries so the shortcuts work on macOS
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        { role: 'delete' },
+        { role: 'selectall' }
+      ]
+    },
+    {
+      // View entries so we can use Dev Tools and Reload
+      label: 'View',
+      submenu: [
+        {role: 'reload'},
+        {role: 'forcereload'},
+        {role: 'toggledevtools'}
+      ]
+    }
+  ]))
+}
