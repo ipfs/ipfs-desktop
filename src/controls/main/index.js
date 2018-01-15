f54f4dda349c74bb7a896af4cb08b24bd00e99d6
Henrique Dias <hacdias@gmail.com>
2018-01-15 15:24:38 +0000

    fix: button to open (#587)

--- src/controls/main/index.js
+++ src/controls/main/index.js
@@ -3,6 +3,7 @@ import downloadHash from './download-hash'
 import fileHistory from './file-history'
 import menuShortcuts from './menu-shortcuts'
 import openFileDialog from './open-file-dialog'
+import openUrl from './open-url'
 import openWebUI from './open-webui'
 import pinnedFiles from './pinned-files'
 import settings from './settings'
@@ -16,6 +17,7 @@ export default function (opts) {
   fileHistory(opts)
   menuShortcuts(opts)
   openFileDialog(opts)
+  openUrl(opts)
   openWebUI(opts)
   pinnedFiles(opts)
   settings(opts)
