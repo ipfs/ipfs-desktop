26eeff88f833d05242cb4acaff36397da09644aa
Henrique Dias <hacdias@gmail.com>
2018-08-24 10:40:19 +0100

    fix: settings page working again
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/controls/main/settings.js
+++ src/controls/main/settings.js
@@ -2,28 +2,65 @@ import {join} from 'path'
 import {shell, ipcMain} from 'electron'
 import { store } from '../../utils'
 
-function openNodeConfig () {
-  return () => {
-    const path = store.get('ipfs').path
-    shell.openItem(join(path, 'config'))
+const openNodeConfig = () => {
+  const path = store.get('ipfs').path
+  shell.openItem(join(path, 'config'))
+}
+
+const settingsToSend = {
+  direct: [
+    'autoLaunch',
+    'screenshotShortcut',
+    'downloadHashShortcut',
+    'lightTheme'
+  ],
+  flags: {
+    dhtClient: '--routing=dhtclient'
   }
 }
 
-function updateSettings () {
-  return (_, key, value) => {
-    store.set(key, value)
+const sendSettings = ({ send }) => () => {
+  const options = {}
+
+  for (const opt of settingsToSend.direct) {
+    options[opt] = store.get(opt, false)
   }
+
+  const flags = store.get('ipfs.flags')
+
+  for (const flag of Object.keys(settingsToSend.flags)) {
+    options[flag] = flags.includes(settingsToSend.flags[flag])
+  }
+
+  send('settings', options)
+}
+
+const updateSettings = (opts) => (_, key, value) => {
+  if (settingsToSend.direct.includes(key)) {
+    store.set(key, value)
+    return sendSettings(opts)()
+  }
+
+  const flags = store.get('ipfs.flags')
+  const flag = settingsToSend.flags[key]
+  const index = flags.indexOf(flag)
+
+  if (value) {
+    if (index === -1) {
+      flags.push(flag)
+    }
+  } else {
+    if (index !== -1) {
+      flags.splice(index, 1)
+    }
+  }
+
+  store.set('ipfs.flags', flags)
+  sendSettings(opts)()
 }
 
 export default function (opts) {
-  const { send } = opts
-
-  const handler = () => {
-    send('settings', store.store)
-  }
-
-  ipcMain.on('request-settings', handler)
-  // TODO: settingsStore.on('change', handler) ?
+  ipcMain.on('request-settings', sendSettings(opts))
   ipcMain.on('update-setting', updateSettings(opts))
-  ipcMain.on('open-node-settings', openNodeConfig(opts))
+  ipcMain.on('open-node-settings', openNodeConfig)
 }
