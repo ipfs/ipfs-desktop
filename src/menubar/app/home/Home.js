74726b2bc8012b27cf4d752e68e9d148f78eafa9
Henrique Dias <hacdias@gmail.com>
2018-10-10 11:08:17 +0100

    wip: separate stuff
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/menubar/app/home/Home.js
+++ src/menubar/app/home/Home.js
@@ -3,32 +3,50 @@ import { ipcRenderer } from 'electron'
 import Button from '../components/button/Button'
 import Summary from './summary/Summary'
 
-export default class Home extends React.Component {
-  render () {
-    const { running, summary, configs } = this.props
+const Running = ({ summary }) => (
+  <div className='pa2'>
+    <Summary {...summary} />
 
-    return (
-      <div>
-        { running ? (
-          <div className='pa2'>
-            <Summary {...summary} />
+    <div className='mt3'>
+      <Button onClick={() => ipcRenderer.send('launchWebUI')} className='f6 w-100'>Open Web UI</Button>
+    </div>
+  </div>
+)
 
-            <div className='mt3'>
-              <Button onClick={() => ipcRenderer.send('launchWebUI')} className='f6 w-100'>Open Web UI</Button>
-            </div>
-          </div>
-        ) : (
-          <div className='pa2'>
-            { Object.keys(configs).length ? (
-              <p>IPFS not running</p>
-            ) : (
-              <div className='tc f6 mt5'>
-                You haven't set up any configuration yet. Head down to <span className='b'>Settings</span> → <span className='b'>Connections</span>.
-              </div>
-            )}
-          </div>
-        )}
-      </div>
-    )
+const NotRunningNoConfig = () => (
+  <div className='tc f6 mt5'>
+    You haven't set up any configuration yet. Head down to <span className='b'>Settings</span> → <span className='b'>Connections</span>.
+  </div>
+)
+
+const NotRunningSingle = () => (
+  <div>
+    Only one config, just run.
+  </div>
+)
+
+const NotRunningMultiple = () => (
+  <div>
+    Multiple configs, pick the one you want.
+  </div>
+)
+
+const Home = (props) => {
+  const { running, configs } = props
+  const configsLength = Object.keys(configs).length
+  let Element
+
+  if (running) {
+    Element = Running
+  } else if (configsLength === 0) {
+    Element = NotRunningNoConfig
+  } else if (configsLength === 1) {
+    Element = NotRunningSingle
+  } else {
+    Element = NotRunningMultiple
   }
+
+  return <Element {...props} />
 }
+
+export default Home
