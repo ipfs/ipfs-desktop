a2f84f1cbef04a55525c676a4e226b9291542ec2
Henrique Dias <hacdias@gmail.com>
2018-11-14 15:17:08 +0000

    chore: remove icons/*.js

--- src/icons/GlyphSmallInfo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SvgGlyphSmallInfo = props => (
-  <svg viewBox='0 0 100 100' {...props}>
-    <path d='M53 69h-5.54V47.2H53zm0-26.53h-5.54v-5.53H53z' />
-  </svg>
-)
-
-export default SvgGlyphSmallInfo
