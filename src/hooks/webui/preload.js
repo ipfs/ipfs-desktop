297451d209dcdacfece0613e7fb547ab717a418c
Henrique Dias <hacdias@gmail.com>
2018-12-08 10:26:57 +0000

    fix: remove useless info

--- src/hooks/webui/preload.js
+++ src/hooks/webui/preload.js
@@ -36,11 +36,3 @@ window.ipfsDesktop = {
     ipcRenderer.send('config.toggle', setting)
   }
 }
-
-// This preload script creates the window.ipfs object with
-// the apiAddress in the URL.
-const urlParams = new URLSearchParams(window.location.search)
-const apiAddress = urlParams.get('api')
-
-// Inject api address
-window.localStorage.setItem('ipfsApi', apiAddress)
