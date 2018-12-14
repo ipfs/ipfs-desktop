84d527103d76622574311a505e99cce44f2245d8
Henrique Dias <hacdias@gmail.com>
2018-12-14 10:04:11 +0000

    feat: more languages (#747)
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -4,9 +4,20 @@ import ICU from 'i18next-icu'
 import Backend from 'i18next-node-fs-backend'
 import LanguageDetector from 'i18next-electron-language-detector'
 
+import cs from 'i18next-icu/locale-data/cs'
+import da from 'i18next-icu/locale-data/da'
 import en from 'i18next-icu/locale-data/en'
+import fr from 'i18next-icu/locale-data/fr'
+import ca from 'i18next-icu/locale-data/ca'
+import ko from 'i18next-icu/locale-data/ko'
+import nl from 'i18next-icu/locale-data/nl'
+import no from 'i18next-icu/locale-data/no'
+import pl from 'i18next-icu/locale-data/pl'
+import zh from 'i18next-icu/locale-data/zh'
+import pt from 'i18next-icu/locale-data/pt'
+import ru from 'i18next-icu/locale-data/ru'
 
-const localeData = [en]
+const localeData = [cs, da, ca, en, fr, ko, nl, no, pl, pt, ru, zh]
 
 i18n
   .use(new ICU({ localeData: localeData }))
