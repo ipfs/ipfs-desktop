b357621b2bfe24e981b2ca3e5c538746574b1fcf
Henrique Dias <hacdias@gmail.com>
2018-12-14 19:32:59 +0000

    fix: prev
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/lib/webui/index.js
+++ src/lib/webui/index.js
@@ -40,7 +40,7 @@ const createWindow = () => {
 export default async function (ctx) {
   const apiAddress = ctx.ipfsd.apiAddr
   const window = createWindow()
-  ctx.sendToWebUI = window.webContents.send
+  ctx.sendToWebUI = (...args) => window.webContents.send(...args)
 
   ipcMain.on('launchWebUI', (_, url) => {
     window.webContents.send('updatedPage', url)
