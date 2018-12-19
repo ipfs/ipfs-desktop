8dfebe5ef55cadebc1c0140714d33406445a52a2
Henrique Dias <hacdias@gmail.com>
2018-12-19 12:49:37 +0000

    feat: two passing tests
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- test/spec.js
+++ test/spec.js
@@ -1,31 +1,64 @@
 /* eslint-env mocha */
 
 const Application = require('spectron').Application
-const assert = require('assert')
 const electronPath = require('electron') // Require Electron from the binaries included in node_modules.
 const path = require('path')
+const fs = require('fs-extra')
+
+const { makeRepository } = require('./utils/ipfsd')
+
+async function startApp (opts) {
+  const app = new Application({
+    path: electronPath,
+    args: ['-r', '@babel/register', path.join(__dirname, '../src/index.js')],
+    ...opts
+  })
+
+  await app.start()
+  return app
+}
 
 describe('Application launch', function () {
-  this.timeout(10000)
+  this.timeout(20000)
 
-  before(function () {
-    this.app = new Application({
-      path: electronPath,
-      args: ['-r', '@babel/register', path.join(__dirname, '../src/index.js')]
+  /* it('starts with no initial repository', async () => {
+    // TODO: how to simulate no initial repo on user Home?
+  }) */
+
+  it('starts with initial repository', async () => {
+    const { dir, ipfsd } = await makeRepository()
+    const app = await startApp({
+      env: {
+        IPFS_PATH: ipfsd.repoPath
+      }
     })
-    return this.app.start()
-  })
 
-  after(function () {
-    if (this.app && this.app.isRunning()) {
-      return this.app.stop()
+    if (app && app.isRunning()) {
+      await app.stop()
     }
+
+    ipfsd.stop()
+    dir.removeCallback()
   })
 
-  it('starts menubar and webui window', function () {
-    assert.ok(this.app.isRunning(), 'App is running')
-    return this.app.client.getWindowCount().then(function (count) {
-      assert.strictEqual(count, 2, 'menubar and webui window exist')
+  it(`starts with repository with 'api' file`, async () => {
+    const { dir, ipfsd } = await makeRepository()
+    const configPath = path.join(ipfsd.repoPath, 'config')
+    const apiPath = path.join(ipfsd.repoPath, 'api')
+    const config = fs.readJsonSync(configPath)
+    fs.writeFile(apiPath, config.Addresses.API)
+
+    const app = await startApp({
+      env: {
+        IPFS_PATH: ipfsd.repoPath
+      }
     })
+
+    if (app && app.isRunning()) {
+      await app.stop()
+    }
+
+    ipfsd.stop()
+    dir.removeCallback()
   })
 })
