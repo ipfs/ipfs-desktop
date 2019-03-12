2335f25d12404be4fa639f97124bd13eafb703e4
Henrique Dias <hacdias@gmail.com>
2019-03-12 21:33:39 +0000

    fix: crash after quit

--- src/lib/webui/index.js
+++ src/lib/webui/index.js
@@ -74,7 +74,7 @@ export default async function (ctx) {
   app.on('before-quit', () => {
     // Makes sure the app quits even though we prevent
     // the closing of this window.
-    if (window) window.destroy()
+    window.removeAllListeners('close')
   })
 
   ipcMain.on('config.get', () => {
