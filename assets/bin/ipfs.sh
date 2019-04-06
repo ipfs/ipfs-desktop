5549261aa405ed603ffbff7cf850f61ba4e2b67b
Henrique Dias <hacdias@gmail.com>
2019-04-06 23:22:39 +0100

    fix: remove unnecessary redirections
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- assets/bin/ipfs.sh
+++ assets/bin/ipfs.sh
@@ -3,4 +3,4 @@
 location=$(cd $(dirname $(test -L "$0" && readlink "$0" || echo "$0"))/../.. && pwd -P)
 ipfs="$location/node_modules/go-ipfs-dep/go-ipfs/ipfs"
 
-$ipfs "$@" 0<&0 1>&1 2>&2
+$ipfs "$@"
