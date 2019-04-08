665e99b601f661b5290b6f7758603d83f9068aac
Oli Evans <oli@tableflip.io>
2019-04-08 11:51:52 +0100

    Update assets/bin/ipfs.sh
    Co-Authored-By: hacdias <hacdias@gmail.com>

--- assets/bin/ipfs.sh
+++ assets/bin/ipfs.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 resources=$(dirname "$(dirname "$(test -L "$0" && readlink "$0" || echo "$0")")")
 ipfs="$resources/app.asar.unpacked/node_modules/go-ipfs-dep/go-ipfs/ipfs"
