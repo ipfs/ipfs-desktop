696671c0fba1b01e8116afd0887ecf23cec62743
Henrique Dias <hacdias@gmail.com>
2019-04-21 23:08:54 +0100

    fix: await on createToggler handler
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/late/utils.js
+++ src/late/utils.js
@@ -2,12 +2,12 @@ import { store } from '../utils'
 import { ipcMain } from 'electron'
 
 export function createToggler ({ webui }, settingsOption, activate) {
-  ipcMain.on('config.toggle', (_, opt) => {
+  ipcMain.on('config.toggle', async (_, opt) => {
     if (opt !== settingsOption) {
       return
     }
 
-    if (activate(store.get(settingsOption))) {
+    if (await activate(store.get(settingsOption))) {
       store.set(settingsOption, !store.get(settingsOption))
     }
 
