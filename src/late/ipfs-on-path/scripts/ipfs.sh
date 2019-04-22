ebb3b04adf6b5d6aaf053ca7dcfb6accf4ed604d
Henrique Dias <hacdias@gmail.com>
2019-04-22 21:40:01 +0100

    fix: ipfs script
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/late/ipfs-on-path/scripts/ipfs.sh
+++ src/late/ipfs-on-path/scripts/ipfs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Get the full path of the app directory (resolving the symlink if needed)
-app=$(dirname $(dirname $(dirname $(dirname "$(dirname "$(test -L "$0" && readlink "$0" || echo "$0")")"))))
+app=$(dirname "$(dirname "$(dirname "$(dirname "$(dirname "$(test -L "$0" && readlink "$0" || echo "$0")")")")")")
 # Get the full path to ipfs binary bundled with ipfs-desktop
 ipfs="$app/node_modules/go-ipfs-dep/go-ipfs/ipfs"
 
