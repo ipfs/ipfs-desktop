09c5884c306f5c356957804d46f1ac4ceb396118
Henrique Dias <hacdias@gmail.com>
2019-05-08 18:54:18 +0100

    feat: more cleanup
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/late/npm-on-ipfs/scripts/npm.js
+++ src/late/npm-on-ipfs/scripts/npm.js
@@ -6,21 +6,19 @@ const { execFile } = require('child_process')
 const { join } = require('path')
 
 readFile(join(homedir, '.ipfs/api'), (err, data) => {
-  if (err) {
-    // TODO: daemon not running? start IPFS Desktop? How?
-    return
-  }
-
   const pkg = process.argv[1].includes('npm') ? 'npm' : 'yarn'
   const addr = data.toString()
-
-  const child = execFile('ipfs-npm', [
+  let args = [
     ...process.argv.slice(2, process.argv.length),
-    '--ipfs-node', addr,
     '--package-manager', join(__dirname, `./${pkg}.bak`)
-  ])
+  ]
 
+  if (!err) {
+    // daemon not running.
+    args.push('--ipfs-node', addr)
+  }
+
+  const child = execFile('ipfs-npm', args)
   child.stdout.pipe(process.stdout)
   child.stderr.pipe(process.stderr)
-  // TODO: HANGS? process.stdin.pipe(child.stdin)
 })
