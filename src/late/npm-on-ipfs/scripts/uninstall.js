908087c8392aee3639023110b22040cb9b779cdb
Henrique Dias <hacdias@gmail.com>
2019-05-09 20:38:21 +0100

    wip: remove code
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/late/npm-on-ipfs/scripts/uninstall.js
+++ src/late/npm-on-ipfs/scripts/uninstall.js
@@ -1,13 +1,1 @@
-const { revert } = require('../../../utils/scripts/backup')
-const which = require('which')
-const argv = require('yargs').argv
-
-const npm = which.sync('npm', { nothrow: true })
-if (!npm) {
-  revert(argv.data, 'npm', npm)
-}
-
-const yarn = which.sync('yarn', { nothrow: true })
-if (!yarn) {
-  revert(argv.data, 'yarn', yarn)
-}
+console.log('UNINSTALL')
