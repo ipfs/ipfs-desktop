0ef4649504882b7886cca94fe13f0bdffd469a4a
Oli Evans <oli@tableflip.io>
2019-05-22 12:59:17 +0100

    chore: delay between tests
    awaiting on stop doesn't seem to be enough to prevent 2 copies of the
    app running side by side under some conditions. add a delay between
    tests.
    
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- test/spec.js
+++ test/spec.js
@@ -45,7 +45,6 @@ describe('Application launch', function () {
 
   it('creates a repository on startup', async function () {
     const { app, home } = await startApp({})
-    this.app = app
 
     const configPath = path.join(home, '.ipfs', 'config')
     expect(app.isRunning()).to.be.true()
@@ -59,31 +58,38 @@ describe('Application launch', function () {
     // ensure strict CORS checking is enabled
     expect(config.API.HTTPHeaders).to.deep.equal({})
     expect(config.Discovery.MDNS.Enabled).to.be.true()
+    await app.stop()
   })
 
-  it('starts with initial repository', async function () {
+  it('starts when external ipfsd is running', async function () {
     const { ipfsd } = await makeRepository()
     const { app } = await startApp({ ipfsPath: ipfsd.repoPath })
-    this.app = app
-    this.ipfsd = ipfsd
+    expect(app.isRunning()).to.be.true()
+    await app.stop()
+    await ipfsd.stop()
   })
 
   it('fixes config for cors checking', async function () {
+    // create config
     const { ipfsd } = await makeRepository()
     const { repoPath } = ipfsd
     await ipfsd.stop()
+
+    // check config has cors disabled
     const configPath = path.join(repoPath, 'config')
     const initConfig = fs.readJsonSync(configPath)
     expect(initConfig.API.HTTPHeaders['Access-Control-Allow-Origin']).to.include('*')
 
     const { app } = await startApp({ ipfsPath: repoPath })
-    this.app = app
+    expect(app.isRunning()).to.be.true()
     delay(5000)
     const config = fs.readJsonSync(configPath)
+    // ensure app has enabled cors checking
     expect(config.API.HTTPHeaders).to.deep.equal({})
+    await app.stop()
   })
 
-  it(`starts with repository with 'api' file`, async function () {
+  it(`starts with repository with 'api' file and no daemon running`, async function () {
     const { ipfsd } = await makeRepository()
     const { repoPath } = ipfsd
     await ipfsd.stop()
@@ -92,16 +98,10 @@ describe('Application launch', function () {
     const config = fs.readJsonSync(configPath)
     fs.writeFile(apiPath, config.Addresses.API)
     const { app } = await startApp({ ipfsPath: ipfsd.repoPath })
-    this.app = app
+    expect(app.isRunning()).to.be.true()
+    await app.stop()
   })
-
   afterEach(async function () {
-    if (this.app && this.app.isRunning()) {
-      await this.app.stop()
-    }
-
-    if (this.ipfsd) {
-      await this.ipfsd.stop()
-    }
+    await delay(3000)
   })
 })
