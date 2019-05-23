dc55df3507f33696cada11c0e1e481b9988734bc
Oli Evans <oli@tableflip.io>
2019-05-23 10:24:20 +0100

    fix: always stop the app
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- test/spec.js
+++ test/spec.js
@@ -18,34 +18,40 @@ const { makeRepository } = require('./utils/ipfsd')
 // const logs = await app.client.getMainProcessLogs()
 // logs.forEach(line => console.log(line))
 //
-
-async function startApp ({
-  home = createTmpDir(),
-  ipfsPath = path.join(home, '.ipfs')
-}) {
-  const app = new Application({
-    path: electronPath,
-    args: ['-r', path.join(__dirname, 'utils/include.js'), path.join(__dirname, '../src/index.js')],
-    env: {
-      NODE_ENV: 'test',
-      HOME: home,
-      IPFS_PATH: ipfsPath
-    }
-  })
-  await app.start()
-  return { app, ipfsPath, home }
-}
-
 function createTmpDir () {
   return tmp.dirSync({ unsafeCleanup: true }).name
 }
 
 describe('Application launch', function () {
   this.timeout(60000)
+  let app = null
+
+  afterEach(async function () {
+    if (app && app.isRunning()) {
+      await app.stop()
+      await delay(3000)
+    }
+  })
+
+  async function startApp ({
+    home = createTmpDir(),
+    ipfsPath = path.join(home, '.ipfs')
+  }) {
+    app = new Application({
+      path: electronPath,
+      args: ['-r', path.join(__dirname, 'utils/include.js'), path.join(__dirname, '../src/index.js')],
+      env: {
+        NODE_ENV: 'test',
+        HOME: home,
+        IPFS_PATH: ipfsPath
+      }
+    })
+    await app.start()
+    return { app, ipfsPath, home }
+  }
 
   it('creates a repository on startup', async function () {
     const { app, home } = await startApp({})
-
     const configPath = path.join(home, '.ipfs', 'config')
     expect(app.isRunning()).to.be.true()
 
@@ -57,14 +63,12 @@ describe('Application launch', function () {
     // ensure strict CORS checking is enabled
     expect(config.API.HTTPHeaders).to.deep.equal({})
     expect(config.Discovery.MDNS.Enabled).to.be.true()
-    await app.stop()
   })
 
   it('starts when external ipfsd is running', async function () {
     const { ipfsd } = await makeRepository()
     const { app } = await startApp({ ipfsPath: ipfsd.repoPath })
     expect(app.isRunning()).to.be.true()
-    await app.stop()
     await ipfsd.stop()
   })
 
@@ -86,15 +90,13 @@ describe('Application launch', function () {
     // ensure app has enabled cors checking
     expect(config.API.HTTPHeaders['Access-Control-Allow-Origin']).to.deep.equal([])
     await app.stop()
-
     // check it doesn't alter the config on second run.
-    // config.API.HTTPHeaders['Access-Control-Allow-Origin'] = ['*']
-    // fs.writeJsonSync(configPath, config, { spaces: 2 })
-    // await app.start()
-    // delay(5000)
-    // config = fs.readJsonSync(configPath)
-    // expect(config.API.HTTPHeaders['Access-Control-Allow-Origin']).to.include('*')
-    // await app.stop()
+    config.API.HTTPHeaders['Access-Control-Allow-Origin'] = ['*']
+    fs.writeJsonSync(configPath, config, { spaces: 2 })
+    await app.start()
+    delay(5000)
+    config = fs.readJsonSync(configPath)
+    expect(config.API.HTTPHeaders['Access-Control-Allow-Origin']).to.include('*')
   })
 
   it('fixes config for cors checking where multiple allowed origins', async function () {
@@ -118,7 +120,6 @@ describe('Application launch', function () {
     // ensure app has enabled cors checking
     const specificOrigins = newOrigins.filter(origin => origin !== '*')
     expect(config.API.HTTPHeaders['Access-Control-Allow-Origin']).to.deep.equal(specificOrigins)
-    await app.stop()
   })
 
   it(`starts with repository with 'api' file and no daemon running`, async function () {
@@ -131,10 +132,5 @@ describe('Application launch', function () {
     fs.writeFile(apiPath, config.Addresses.API)
     const { app } = await startApp({ ipfsPath: ipfsd.repoPath })
     expect(app.isRunning()).to.be.true()
-    await app.stop()
-  })
-
-  afterEach(async function () {
-    await delay(3000)
   })
 })
