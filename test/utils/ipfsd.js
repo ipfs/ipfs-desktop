f1f6c3969b61a86c3f644ba037459513dfefaa97
Henrique Dias <hacdias@gmail.com>
2019-07-06 11:48:09 +0100

    chore: update dependencies (#970)
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- test/utils/ipfsd.js
+++ test/utils/ipfsd.js
@@ -7,23 +7,17 @@ async function makeRepository () {
   const dir = tmp.dirSync({ unsafeCleanup: true })
   const factory = IPFSFactory.create({ type: 'go' })
 
-  const ipfsd = await new Promise((resolve, reject) => {
-    factory.spawn({
-      disposable: false,
-      repoPath: dir.name,
-      init: false,
-      start: false
-    }, function (err, ipfsd) {
-      if (err) return reject(err)
-      ipfsd.init({
-        bits: 1024,
-        profile: 'test',
-        directory: dir.name
-      }, e => {
-        if (e) return reject(e)
-        resolve(ipfsd)
-      })
-    })
+  const ipfsd = await factory.spawn({
+    disposable: false,
+    repoPath: dir.name,
+    init: false,
+    start: false
+  })
+
+  await ipfsd.init({
+    bits: 1024,
+    profile: 'test',
+    directory: dir.name
   })
 
   return { ipfsd, dir }
