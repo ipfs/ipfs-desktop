a6fcac620853d857c329a669963cc84767ca5c76
Henrique Dias <hacdias@gmail.com>
2019-07-15 10:59:37 +0000

    fix: always try without perms, if fail, sudo (#969)
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/utils/exec-or-sudo.js
+++ src/utils/exec-or-sudo.js
@@ -1,57 +1,69 @@
-import os from 'os'
 import i18n from 'i18next'
 import sudo from 'sudo-prompt'
 import { dialog, app } from 'electron'
-import { execFile } from 'child_process'
 import logger from './logger'
 import { recoverableErrorDialog } from '../dialogs'
+import util from 'util'
 
-export default async function (script, scope) {
+const execFile = util.promisify(require('child_process').execFile)
+
+const env = {
+  noSudo: {
+    ELECTRON_RUN_AS_NODE: 1
+  },
+  sudo: 'env ELECTRON_RUN_AS_NODE=1'
+}
+
+const getResult = (err, stdout, stderr, scope) => {
+  if (stdout) {
+    logger.info(`[${scope}] sudo: stdout: ${stdout.toString().trim()}`)
+  }
+
+  if (stderr) {
+    logger.info(`[${scope}] sudo: stderr: ${stderr.toString().trim()}`)
+  }
+
+  if (!err) {
+    return true
+  }
+
+  const str = err.toString()
+  logger.error(`[${scope}] error: ${str}`)
+
+  if (process.env.NODE_ENV !== 'test') {
+    if (str.includes('No polkit authentication agent found')) {
+      dialog.showErrorBox(i18n.t('polkitDialog.title'), i18n.t('polkitDialog.message'))
+    } else if (str.includes('User did not grant permission')) {
+      dialog.showErrorBox(i18n.t('noPermissionDialog.title'), i18n.t('noPermissionDialog.message'))
+    } else {
+      recoverableErrorDialog(err)
+    }
+  }
+
+  return false
+}
+
+export default async function ({ script, scope, trySudo = true }) {
   const dataArg = `--data="${app.getPath('userData')}"`
 
-  const getResult = (err, stdout, stderr) => {
-    if (stdout) {
-      logger.info(`[${scope}] stdout: ${stdout.toString().trim()}`)
-    }
+  // First try executing with regular permissions.
+  try {
+    const { stdout } = await execFile(process.execPath, [script, dataArg], { env: env.noSudo })
+    logger.info(`[${scope}] stdout: ${stdout.toString().trim()}`)
+    return true
+  } catch ({ stderr }) {
+    logger.info(`[${scope}] no-sudo: stderr: ${stderr.toString().trim()}`)
+  }
 
-    if (stderr) {
-      logger.info(`[${scope}] stderr: ${stderr.toString().trim()}`)
-    }
-
-    if (!err) {
-      return true
-    }
-
-    const str = err.toString()
-    logger.error(`[${scope}] error: ${str}`)
-
-    if (process.env.NODE_ENV !== 'test') {
-      if (str.includes('No polkit authentication agent found')) {
-        dialog.showErrorBox(i18n.t('polkitDialog.title'), i18n.t('polkitDialog.message'))
-      } else if (str.includes('User did not grant permission')) {
-        dialog.showErrorBox(i18n.t('noPermissionDialog.title'), i18n.t('noPermissionDialog.message'))
-      } else {
-        recoverableErrorDialog(err)
-      }
-    }
-
+  if (!trySudo) {
     return false
   }
 
+  // Otherwise, try to elevate the user.
+  const command = `${env.sudo} "${process.execPath}" "${script}" ${dataArg}`
   return new Promise(resolve => {
-    if (os.platform() === 'darwin') {
-      return execFile(process.execPath, [script, dataArg], {
-        env: {
-          ELECTRON_RUN_AS_NODE: 1
-        }
-      }, (err, stdout, stderr) => {
-        resolve(getResult(err, stdout, stderr))
-      })
-    }
-
-    const command = `env ELECTRON_RUN_AS_NODE=1 "${process.execPath}" "${script}" ${dataArg}`
     sudo.exec(command, { name: 'IPFS Desktop' }, (err, stdout, stderr) => {
-      resolve(getResult(err, stdout, stderr))
+      resolve(getResult(err, stdout, stderr, scope))
     })
   })
 }
