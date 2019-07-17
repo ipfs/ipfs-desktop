6a154f40ba0052797dc2c857eb14d583c62a09eb
Henrique Dias <hacdias@gmail.com>
2019-07-17 21:24:16 +0100

    working this way
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/utils/move-repository-location.js
+++ src/utils/move-repository-location.js
@@ -1,16 +1,17 @@
 import { showDialog } from '../dialogs'
 import i18n from 'i18next'
+import { app } from 'electron'
 import selectDirectory from './select-directory'
 
 export default async function () {
-  const opt = showDialog({
+  /* const opt = showDialog({
     title: i18n.t('moveRepositoryLocation'),
     message: i18n.t('moveRepositoryLocation')
   })
 
   if (opt !== 0) {
     return
-  }
+  } */
 
   const dir = await selectDirectory()
 
