14b807f83698d952e8751ea950b1fff1739c79ff
Henrique Dias <hacdias@gmail.com>
2019-07-19 10:04:20 +0200

    wip

--- src/utils/move-repository-location.js
+++ src/utils/move-repository-location.js
@@ -1,9 +1,13 @@
 import { showDialog } from '../dialogs'
 import i18n from 'i18next'
 import { app } from 'electron'
+
+import store from './store'
+import path from 'path'
+import fs from 'fs-extra'
 import selectDirectory from './select-directory'
 
-export default async function () {
+export default async function ({ stopIpfs, startIpfs }) {
   /* const opt = showDialog({
     title: i18n.t('moveRepositoryLocation'),
     message: i18n.t('moveRepositoryLocation')
@@ -19,5 +23,19 @@ export default async function () {
     return
   }
 
+  await stopIpfs()
+
+  const config = store.get('ipfsConfig')
+
+  const currDir = config.path
+  const currName = path.basename(currDir)
+  const newDir = path.join(dir, currName)
+
+  await fs.move(currDir, newDir)
+
+  config.path = newDir
+  store.set('ipfsConfig', config)
+
+  await startIpfs()
   console.log(dir)
 }
