bef0c47b44b2a42d0d3dcb270f62361ec4eabcf2
Henrique Dias <hacdias@gmail.com>
2019-07-22 19:38:48 +0200

    fix: handle port collision gracefully (#989)
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/utils/daemon.js
+++ src/utils/daemon.js
@@ -8,6 +8,9 @@ import { showDialog } from '../dialogs'
 import { execFileSync } from 'child_process'
 import findExecutable from 'ipfsd-ctl/src/utils/find-ipfs-executable'
 import store from './store'
+import multiaddr from 'multiaddr'
+import http from 'http'
+import getPort from 'get-port'
 
 function cannotConnectDialog (addr) {
   showDialog({
@@ -139,10 +142,118 @@ function checkCorsConfig (ipfsd) {
   store.set('checkedCorsConfig', true)
 }
 
+async function checkIfAddrIsDaemon (addr) {
+  const options = {
+    method: 'GET',
+    host: addr.address,
+    port: addr.port,
+    path: '/api/v0/refs?arg=/ipfs/QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn'
+  }
+
+  return new Promise(resolve => {
+    var req = http.request(options, function (r) {
+      resolve(r.statusCode === 200)
+    })
+
+    req.on('error', () => {
+      resolve(false)
+    })
+
+    req.end()
+  })
+}
+
+const parseCfgMultiaddr = (addr) => (addr.includes('/http')
+  ? multiaddr(addr)
+  : multiaddr(addr).encapsulate('/http')
+)
+
+async function checkPorts (ipfsd) {
+  const config = readConfigFile(ipfsd)
+
+  const configApiMa = parseCfgMultiaddr(config.Addresses.API)
+  const configGatewayMa = parseCfgMultiaddr(config.Addresses.Gateway)
+
+  const isApiMaDaemon = await checkIfAddrIsDaemon(configApiMa.nodeAddress())
+  const isGatewayMaDaemon = await checkIfAddrIsDaemon(configGatewayMa.nodeAddress())
+
+  if (isApiMaDaemon && isGatewayMaDaemon) {
+    logger.info('[daemon] ports busy by a daemon')
+    return true
+  }
+
+  const apiPort = parseInt(configApiMa.nodeAddress().port, 10)
+  const gatewayPort = parseInt(configGatewayMa.nodeAddress().port, 10)
+
+  const freeGatewayPort = await getPort({ port: getPort.makeRange(gatewayPort, gatewayPort + 100) })
+  const freeApiPort = await getPort({ port: getPort.makeRange(apiPort, apiPort + 100) })
+
+  const busyApiPort = apiPort !== freeApiPort
+  const busyGatewayPort = gatewayPort !== freeGatewayPort
+
+  if (!busyApiPort && !busyGatewayPort) {
+    return
+  }
+
+  let message = null
+  let options = null
+
+  if (busyApiPort && busyGatewayPort) {
+    logger.info('[daemon] api and gateway ports busy')
+    message = 'busyPortsDialog'
+    options = {
+      port1: apiPort,
+      alt1: freeApiPort,
+      port2: gatewayPort,
+      alt2: freeGatewayPort
+    }
+  } else if (busyApiPort) {
+    logger.info('[daemon] api port busy')
+    message = 'busyPortDialog'
+    options = {
+      port: apiPort,
+      alt: freeApiPort
+    }
+  } else {
+    logger.info('[daemon] gateway port busy')
+    message = 'busyPortDialog'
+    options = {
+      port: gatewayPort,
+      alt: freeGatewayPort
+    }
+  }
+
+  const opt = showDialog({
+    title: i18n.t(`${message}.title`),
+    message: i18n.t(`${message}.message`, options),
+    type: 'error',
+    buttons: [
+      i18n.t(`${message}.action`, options),
+      i18n.t('close')
+    ]
+  })
+
+  if (opt !== 0) {
+    throw new Error('ports already being used')
+  }
+
+  if (busyApiPort) {
+    config.Addresses.API = config.Addresses.API.replace(apiPort.toString(), freeApiPort.toString())
+  }
+
+  if (busyGatewayPort) {
+    config.Addresses.Gateway = config.Addresses.Gateway.replace(gatewayPort.toString(), freeGatewayPort.toString())
+  }
+
+  writeConfigFile(ipfsd, config)
+  logger.info('[daemon] ports updated')
+}
+
 export default async function (opts) {
   const ipfsd = await spawn(opts)
 
   if (!ipfsd.started) {
+    await checkPorts(ipfsd)
     await ipfsd.start(opts.flags)
   }
 
