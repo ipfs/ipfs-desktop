61cf0f8bd9c6ed30abdab1c1397e2042976abd96
Henrique Dias <hacdias@gmail.com>
2019-08-06 14:49:35 +0100

    fix: escape path
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/ipfs-on-path/scripts/ipfs.sh
+++ src/ipfs-on-path/scripts/ipfs.sh
@@ -2,12 +2,12 @@
 
 dir="$(dirname "$(test -L "$0" && readlink "$0" || echo "$0")")"
 
-if [ -f $dir/IPFS_PATH ]; then
-  export IPFS_PATH="$(cat $dir/IPFS_PATH)"
+if [ -f "$dir/IPFS_PATH" ]; then
+  export IPFS_PATH="$(cat "$dir/IPFS_PATH")"
 fi
 
 # Get the full path of the app directory (resolving the symlink if needed)
-app=$(dirname "$(dirname "$(dirname $dir)")")
+app=$(dirname "$(dirname "$(dirname "$dir")")")
 # Get the full path to ipfs binary bundled with ipfs-desktop
 ipfs="$app/node_modules/go-ipfs-dep/go-ipfs/ipfs"
 
