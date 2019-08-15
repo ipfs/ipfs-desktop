088936e0fb3cb02504d8cdfadd4707de842ba79a
Henrique Dias <hacdias@gmail.com>
2019-08-15 08:59:51 +0100

    fix: use go-ipfs version from package.json section
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/common/consts.js
+++ src/common/consts.js
@@ -1,4 +1,12 @@
 import os from 'os'
+import packageJson from '../../package.json'
 
 export const IS_MAC = os.platform() === 'darwin'
+
 export const IS_WIN = os.platform() === 'win32'
+
+export const VERSION = packageJson.version
+
+export const GO_IPFS_VERSION = packageJson['go-ipfs']
+  ? packageJson['go-ipfs'].version.slice(1)
+  : packageJson.dependencies['go-ipfs-dep']
