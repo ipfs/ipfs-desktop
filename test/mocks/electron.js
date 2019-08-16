b696fd14910518c6ab965ae949c488005b075e31
Henrique Dias <hacdias@gmail.com>
2019-08-16 08:51:23 +0100

    feat: add dock test

--- test/mocks/electron.js
+++ test/mocks/electron.js
@@ -1,7 +1,23 @@
 import { EventEmitter } from 'events'
+import sinon from 'sinon'
 
-export default function mockElectron () {
-  return {
-    ipcMain: new EventEmitter()
+export default function mockElectron (opts = {}) {
+  opts.withDock = opts.withDock || false
+
+  const electron = {
+    ipcMain: new EventEmitter(),
+    BrowserWindow: {
+      getAllWindows: sinon.stub()
+    },
+    app: {}
   }
+
+  if (opts.withDock) {
+    electron.app.dock = {
+      show: sinon.spy(),
+      hide: sinon.spy()
+    }
+  }
+
+  return electron
 }
