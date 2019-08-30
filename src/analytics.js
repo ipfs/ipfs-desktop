3985cdf6a97557502680b3c381a24377161d1dc7
Henrique Dias <hacdias@gmail.com>
2019-08-30 23:29:04 +0100

    fix: add analytics.js
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

---
+++ src/analytics.js
@@ -0,0 +1,28 @@
+import Countly from 'countly-sdk-nodejs'
+import { app, ipcMain } from 'electron'
+import { COUNTLY_KEY } from './common/consts'
+
+export default async function (ctx) {
+  Countly.init({
+    url: 'https://countly.ipfs.io',
+    app_key: COUNTLY_KEY,
+    debug: process.env.NODE_ENV === 'development',
+    requireConsent: true
+  })
+
+  Countly.begin_session()
+
+  ctx.countlyDeviceId = Countly.device_id
+
+  ipcMain.on('countly.addConsent', (_, consent) => {
+    Countly.add_consent(consent)
+  })
+
+  ipcMain.on('countly.removeConsent', (_, consent) => {
+    Countly.remove_consent(consent)
+  })
+
+  app.on('before-quit', async e => {
+    Countly.end_session()
+  })
+}
