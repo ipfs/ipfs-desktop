2d7e38ce37bfafcf0b0f054f86f259797e642429
Henrique Dias <hacdias@gmail.com>
2019-09-02 13:39:46 +0100

    fix: increased window draggable area
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/webui/preload.js
+++ src/webui/preload.js
@@ -86,14 +86,3 @@ const apiAddress = urlParams.get('api')
 
 // Inject api address
 window.localStorage.setItem('ipfsApi', apiAddress)
-
-// Allow webui window to be dragged on mac
-document.addEventListener('DOMContentLoaded', function (event) {
-  var windowTopBar = document.createElement('div')
-  windowTopBar.style.width = '100%'
-  windowTopBar.style.height = '32px'
-  windowTopBar.style.position = 'absolute'
-  windowTopBar.style.top = windowTopBar.style.left = 0
-  windowTopBar.style.webkitAppRegion = 'drag'
-  document.body.appendChild(windowTopBar)
-})
