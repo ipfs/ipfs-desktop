b39cdfc2151dc80376b3d9bd463bd7d9fe0db76c
Henrique Dias <hacdias@gmail.com>
2019-09-03 07:51:31 +0100

    fix: move IPFS_PATH to ~/.ipfs-desktop
    This fixes the issue caused by having IPFS_PATH in the same
    directory as the app code. That would fail in multiple circunstances:
    
    1. When the app was updated, IPFS_PATH would get deleted.
    2. macOS gatekeeper paths randomization for security purposes creates
    a read-only directory for the app files when the app isn't executed
    from secure locations (such as ~/Applications) and it would make it fail.
    
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/ipfs-on-path/scripts/bin-win/ipfs.cmd
+++ src/ipfs-on-path/scripts/bin-win/ipfs.cmd
@@ -1,7 +1,7 @@
 @echo off
 
-if exist "%~dp0\..\IPFS_PATH" (
-  SET /P IPFS_PATH=<"%~dp0\..\IPFS_PATH"
+if exist "%USERPROFILE%\.ipfs-desktop\IPFS_PATH" (
+  SET /P IPFS_PATH=<"%USERPROFILE%\.ipfs-desktop\IPFS_PATH"
 )
 
 "%~dp0\..\..\..\..\node_modules\go-ipfs-dep\go-ipfs\ipfs.exe" %*
