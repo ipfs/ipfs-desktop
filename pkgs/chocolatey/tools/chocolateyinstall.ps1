8f620fb1b07b934814530e15d5efc3f8541aeaf9
Henrique Dias <hacdias@gmail.com>
2019-09-30 17:35:04 +0100

    chore: update choco [ci skip]
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- pkgs/chocolatey/tools/chocolateyinstall.ps1
+++ pkgs/chocolatey/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 
-$url = 'https://github.com/ipfs-shipyard/ipfs-desktop/releases/download/v0.9.3/IPFS-Desktop-Setup-0.9.3.exe'
-$checksum = '452A42DA3E7CA7C2F89512254F95664ED8C03D780A4BC276497109E5DCEAECB4'
+$url = 'https://github.com/ipfs-shipyard/ipfs-desktop/releases/download/v0.9.4/IPFS-Desktop-Setup-0.9.4.exe'
+$checksum = 'F108516DD6ABBF27B2D165B606D8ADBF84B39F1B26B5B7292BEB2B70916D8101'
 
 $packageArgs = @{
   packageName   = 'ipfs-desktop'
