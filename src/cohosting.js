30774af213ac57262b53db3fbacc707da33a1b7e
Henrique Dias <hacdias@gmail.com>
2019-10-06 09:44:16 +0100

    feat: use cohost from ipfs-cohost
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/cohosting.js
+++ src/cohosting.js
@@ -1,17 +1,11 @@
 import logger from './common/logger'
+import cohost from 'ipfs-cohost'
 
 // Cohosting is an experiment based on MFS. IPFS Desktop
 // only needs to update the snapshots from time to time.
 //
 // Please see the spec and other discussion on
 // https://github.com/ipfs-shipyard/cohosting
-
-function getTimestamp () {
-  return new Date().toISOString()
-    .replace(/:/g, '')
-    .replace('T', '_')
-    .split('.')[0]
-}
 
 async function update (ctx) {
   const ipfsd = await ctx.getIpfsd()
@@ -23,27 +17,7 @@ async function update (ctx) {
   const ipfs = ipfsd.api
 
   try {
-    await ipfs.files.stat('/cohosting')
-    const files = await ipfs.files.ls('/cohosting')
-    const domains = files.map(file => file.name)
-
-    for (const domain of domains) {
-      const cid = await ipfs.resolve(`/ipns/${domain}`)
-      const dirs = await ipfs.files.ls(`/cohosting/${domain}`)
-      const latest = dirs.map(file => file.name).sort().pop()
-
-      if (latest) {
-        const path = `/cohosting/${domain}/${latest}`
-        const stat = await ipfs.files.stat(path)
-
-        if (`/ipfs/${stat.hash}` === cid) {
-          await ipfs.files.mv([path, `/cohosting/${domain}/${getTimestamp()}`])
-          continue
-        }
-      }
-
-      await ipfs.files.cp([cid, `/cohosting/${domain}/${getTimestamp()}`])
-    }
+    cohost.sync(ipfs)
   } catch (err) {
     logger.error(`[cohosting] ${err.toString()}`)
   }
