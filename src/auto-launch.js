c34ee7e61d7867753e134d830bff8599830119ca
Henrique Dias <hacdias@gmail.com>
2019-10-18 19:01:51 +0100

    feat: improve auto launch
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/auto-launch.js
+++ src/auto-launch.js
@@ -1,29 +1,77 @@
 import AutoLaunch from 'auto-launch'
+import { app } from 'electron'
+import os from 'os'
+import path from 'path'
+import fs from 'fs-extra'
+import untildify from 'untildify'
 import createToggler from './create-toggler'
 import logger from './common/logger'
 import store from './common/store'
 
 const CONFIG_KEY = 'autoLaunch'
 
+function isSupported () {
+  const plat = os.platform()
+  return plat === 'linux' || plat === 'win32' || plat === 'darwin'
+}
+
+function getDesktopFile () {
+  return path.join(untildify('~/.config/autostart/'), 'ipfs-desktop.desktop')
+}
+
 const autoLauncher = new AutoLaunch({
   name: 'IPFS Desktop'
 })
 
-export default function (ctx) {
+async function enable () {
+  if (app.setLoginItemSettings) {
+    app.setLoginItemSettings({ openAtLogin: true })
+  }
+
+  const desktop = `[Desktop Entry]
+Type=Application
+Version=1.0
+Name="IPFS Desktop"
+Comment="IPFS Desktop Startup Script"
+Exec="${process.execPath}"
+StartupNotify=false
+Terminal=false`
+
+  await fs.outputFile(getDesktopFile(), desktop)
+}
+
+async function disable () {
+  if (app.setLoginItemSettings) {
+    app.setLoginItemSettings({ openAtLogin: false })
+  }
+
+  await fs.remove(getDesktopFile())
+}
+
+export default async function (ctx) {
+  // Disable the old auto launch mechanism.
+  // TODO: remove on 0.10.0.
+  if (await autoLauncher.isEnabled()) await autoLauncher.disable()
+
   const activate = async (value, oldValue) => {
     if (process.env.NODE_ENV === 'development') {
       logger.info('[launch on startup] unavailable during development')
       return
     }
 
+    if (!isSupported()) {
+      logger.info('[launch on startup] not supported on this platform')
+      return false
+    }
+
     if (value === oldValue) return
 
     try {
       if (value === true) {
-        if (!await autoLauncher.isEnabled()) await autoLauncher.enable()
+        await enable()
         logger.info('[launch on startup] enabled')
       } else {
-        if (await autoLauncher.isEnabled()) await autoLauncher.disable()
+        await disable()
         logger.info('[launch on startup] disabled')
       }
 
