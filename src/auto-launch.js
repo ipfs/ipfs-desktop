6a754d15d47c685608462afb326afd912dafb8d7
Henrique Dias <hacdias@gmail.com>
2019-10-21 15:22:47 +0100

    feat: improve auto launch at login (#1200)
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/auto-launch.js
+++ src/auto-launch.js
@@ -1,29 +1,88 @@
 import AutoLaunch from 'auto-launch'
+import { app } from 'electron'
+import os from 'os'
+import path from 'path'
+import fs from 'fs-extra'
+import untildify from 'untildify'
 import createToggler from './create-toggler'
 import logger from './common/logger'
 import store from './common/store'
 
 const CONFIG_KEY = 'autoLaunch'
 
-const autoLauncher = new AutoLaunch({
-  name: 'IPFS Desktop'
-})
+function isSupported () {
+  const plat = os.platform()
+  return plat === 'linux' || plat === 'win32' || plat === 'darwin'
+}
 
-export default function (ctx) {
+// Disable the old auto launch mechanism.
+// TODO: remove on 0.10.0.
+async function disableOldLogin () {
+  try {
+    const autoLauncher = new AutoLaunch({ name: 'IPFS Desktop' })
+
+    if (await autoLauncher.isEnabled()) {
+      await autoLauncher.disable()
+      logger.error('[launch on startup] old mechanism disabled')
+    }
+  } catch (_) {
+    // ignore...
+  }
+}
+
+function getDesktopFile () {
+  return path.join(untildify('~/.config/autostart/'), 'ipfs-desktop.desktop')
+}
+
+async function enable () {
+  if (app.setLoginItemSettings) {
+    app.setLoginItemSettings({ openAtLogin: true })
+    return
+  }
+
+  const desktop = `[Desktop Entry]
+Type=Application
+Version=1.0
+Name=IPFS Desktop
+Comment=IPFS Desktop Startup Script
+Exec="${process.execPath}"
+StartupNotify=false
+Terminal=false`
+
+  await fs.outputFile(getDesktopFile(), desktop)
+}
+
+async function disable () {
+  if (app.setLoginItemSettings) {
+    app.setLoginItemSettings({ openAtLogin: false })
+    return
+  }
+
+  await fs.remove(getDesktopFile())
+}
+
+export default async function (ctx) {
+  await disableOldLogin()
+
   const activate = async (value, oldValue) => {
     if (process.env.NODE_ENV === 'development') {
       logger.info('[launch on startup] unavailable during development')
       return
     }
 
+    if (!isSupported()) {
+      logger.info('[launch on startup] not supported on this platform')
+      return false
+    }
+
     if (value === oldValue) return
 
     try {
       if (value === true) {
-        if (!await autoLauncher.isEnabled()) await autoLauncher.enable()
+        await enable()
         logger.info('[launch on startup] enabled')
       } else {
-        if (await autoLauncher.isEnabled()) await autoLauncher.disable()
+        await disable()
         logger.info('[launch on startup] disabled')
       }
 
