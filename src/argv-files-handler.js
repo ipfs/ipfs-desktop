c12e18b619230350620c79253bdabacd0dffc3d5
Henrique Dias <hacdias@gmail.com>
2019-10-29 14:46:38 +0000

    feat: copy link to clipboard when adding to ipfs (#1221)
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/argv-files-handler.js
+++ src/argv-files-handler.js
@@ -4,6 +4,7 @@ import addToIpfs from './add-to-ipfs'
 export async function argvHandler (argv, ctx) {
   let handled = false
 
+  const files = []
   for (const arg of argv.slice(1)) {
     if (!arg.startsWith('--add')) {
       continue
@@ -12,9 +13,13 @@ export async function argvHandler (argv, ctx) {
     const filename = arg.slice(6)
 
     if (await fs.pathExists(filename)) {
-      await addToIpfs(ctx, filename)
-      handled = true
+      files.push(filename)
     }
+  }
+
+  if (files.length > 0) {
+    await addToIpfs(ctx, files)
+    handled = true
   }
 
   return handled
