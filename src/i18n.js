142729e0f8d5035d3dc58b39b5be00c27641c89e
Marcin Rataj <lidel@lidel.org>
2020-02-10 12:11:30 +0100

    chore(i18n): sync locale with Transifex
    License: MIT
    Signed-off-by: Marcin Rataj <lidel@lidel.org>

--- src/i18n.js
+++ src/i18n.js
@@ -5,13 +5,17 @@ import ICU from 'i18next-icu'
 import Backend from 'i18next-node-fs-backend'
 import store from './common/store'
 
+import ar from 'i18next-icu/locale-data/ar'
 import ca from 'i18next-icu/locale-data/ca'
 import cs from 'i18next-icu/locale-data/cs'
 import da from 'i18next-icu/locale-data/da'
 import de from 'i18next-icu/locale-data/de'
+import el from 'i18next-icu/locale-data/el'
 import eo from 'i18next-icu/locale-data/eo'
 import es from 'i18next-icu/locale-data/es'
 import en from 'i18next-icu/locale-data/en'
+import et from 'i18next-icu/locale-data/et'
+import fa from 'i18next-icu/locale-data/fa'
 import fr from 'i18next-icu/locale-data/fr'
 import fi from 'i18next-icu/locale-data/fi'
 import he from 'i18next-icu/locale-data/he'
@@ -21,6 +25,8 @@ import hu from 'i18next-icu/locale-data/hu'
 import it from 'i18next-icu/locale-data/it'
 import ja from 'i18next-icu/locale-data/ja'
 import ko from 'i18next-icu/locale-data/ko'
+import lt from 'i18next-icu/locale-data/lt'
+import lv from 'i18next-icu/locale-data/lv'
 import nl from 'i18next-icu/locale-data/nl'
 import no from 'i18next-icu/locale-data/no'
 import pl from 'i18next-icu/locale-data/pl'
@@ -30,10 +36,15 @@ import sk from 'i18next-icu/locale-data/sk'
 import sl from 'i18next-icu/locale-data/sl'
 import sr from 'i18next-icu/locale-data/sr'
 import sv from 'i18next-icu/locale-data/sv'
+import tr from 'i18next-icu/locale-data/tr'
 import uk from 'i18next-icu/locale-data/uk'
+import uz from 'i18next-icu/locale-data/uz'
 import zh from 'i18next-icu/locale-data/zh'
 
-const localeData = [ca, cs, da, de, eo, es, en, fi, fr, he, hi, hr, hu, it, ja, ko, nl, no, pl, pt, ru, sk, sl, sr, sv, uk, zh]
+const localeData = [
+  ar, ca, cs, da, de, el, eo, es, en, et, fa, fi, fr, he, hi, hr, hu, it,
+  ja, ko, lt, lv, nl, no, pl, pt, ru, sk, sl, sr, sv, tr, uk, uz, zh
+]
 
 export default async function () {
   await i18n
