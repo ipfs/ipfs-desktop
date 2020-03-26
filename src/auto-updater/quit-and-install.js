ba6f799cc4b3ef138c5b7ae97dcb495398f47563
Henrique Dias <hacdias@gmail.com>
2020-03-26 15:55:21 +0000

    fix: call stopIpfs on ctx
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/auto-updater/quit-and-install.js
+++ src/auto-updater/quit-and-install.js
@@ -3,14 +3,14 @@ import { autoUpdater } from 'electron-updater'
 import { STATUS } from '../daemon'
 
 // adapted from https://github.com/electron-userland/electron-builder/issues/1604#issuecomment-372091881
-export default async function quitAndInstall ({ stopIpfs }) {
+export default async function quitAndInstall (ctx) {
   app.removeAllListeners('window-all-closed')
   const browserWindows = BrowserWindow.getAllWindows()
   browserWindows.forEach(function (browserWindow) {
     browserWindow.removeAllListeners('close')
   })
 
-  const status = await stopIpfs()
+  const status = await ctx.stopIpfs()
 
   if (status === STATUS.STOPPING_FAILED || status === STATUS.STOPPING_FINISHED) {
     autoUpdater.quitAndInstall(true, true)
