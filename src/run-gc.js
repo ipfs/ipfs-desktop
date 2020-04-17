f1a66084950a83a7ad1a0c4c6915d86ec38ece15
Henrique Dias <hacdias@gmail.com>
2020-04-17 10:26:01 +0100

    feat: run gc
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

---
+++ src/run-gc.js
@@ -0,0 +1,48 @@
+const i18n = require('i18next')
+const logger = require('./common/logger')
+const { showDialog, recoverableErrorDialog } = require('./dialogs')
+const dock = require('./dock')
+
+module.exports = function runGarbageCollector ({ getIpfsd }) {
+  dock.run(async () => {
+    logger.info('[run gc] alerting user for effects')
+
+    const opt = showDialog({
+      title: i18n.t('runGarbageCollectorWarning.title'),
+      message: i18n.t('runGarbageCollectorWarning.message'),
+      type: 'warning',
+      buttons: [
+        i18n.t('runGarbageCollectorWarning.action'),
+        i18n.t('cancel')
+      ],
+      showDock: false
+    })
+
+    if (opt !== 0) {
+      logger.info('[run gc] user canceled')
+      return
+    }
+
+    const ipfsd = await getIpfsd()
+
+    if (!ipfsd) {
+      return
+    }
+
+    try {
+      ipfsd.api.repo.gc()
+      showDialog({
+        title: i18n.t('runGarbageCollectorDone.title'),
+        message: i18n.t('runGarbageCollectorDone.message'),
+        type: 'info',
+        buttons: [
+          i18n.t('ok')
+        ],
+        showDock: false
+      })
+    } catch (err) {
+      logger.error(`[run gc] ${err.toString()}`)
+      return recoverableErrorDialog(err)
+    }
+  })
+}
