1f42140448e09e122d3ea6bb5b67b9d6d7ce15dd
Henrique Dias <hacdias@gmail.com>
2020-04-23 13:15:40 +0100

    fix(BREAKING): ipfs daemon flags (#1437)
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/common/store.js
+++ src/common/store.js
@@ -3,18 +3,30 @@ const Store = require('electron-store')
 
 const store = new Store()
 
-if (store.get('version') !== 5) {
+const defaultFlags = [
+  '--migrate',
+  '--enable-gc',
+  '--routing', 'dhtclient'
+]
+
+if (store.get('version', 0) < 5) {
   store.clear()
 
   // default config
   store.set('ipfsConfig', {
     type: 'go',
     path: '',
-    flags: ['--migrate=true', '--routing=dhtclient', '--enable-gc=true'],
+    flags: defaultFlags,
     keysize: 2048
   })
 
   store.set('version', 5)
+}
+
+const flags = store.get('ipfsConfig.flags', [])
+
+if (flags.includes('--migrate=true') || flags.includes('--enable-gc=true')) {
+  store.set('ipfsConfig.flags', defaultFlags)
 }
 
 if (!store.get('language')) {
