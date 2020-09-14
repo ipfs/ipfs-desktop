d1ab391b54fe7b3c138c81f09089b030bd8532b3
Marcin Rataj <lidel@lidel.org>
2020-09-14 22:00:33 +0200

    chore: electron 9 (#1641)
    This updates to Electron v9.3.0 and adjusts CORS logic
    to account for upstream changes.

--- src/common/consts.js
+++ src/common/consts.js
@@ -8,6 +8,7 @@ module.exports = Object.freeze({
   GO_IPFS_VERSION: packageJson['go-ipfs']
     ? packageJson['go-ipfs'].version.slice(1)
     : packageJson.dependencies['go-ipfs-dep'],
+  ELECTRON_VERSION: packageJson.dependencies.electron,
   COUNTLY_KEY: process.env.NODE_ENV === 'development'
     ? '6b00e04fa5370b1ce361d2f24a09c74254eee382'
     : '47fbb3db3426d2ae32b3b65fe40c564063d8b55d'
