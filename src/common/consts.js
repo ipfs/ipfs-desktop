bc5db4592428aa225a63ab86b63d7467e24d2ca8
Rafael Ramalho <rafazelramalho19@gmail.com>
2020-09-30 12:02:38 +0100

    feat: go-ipfs v0.7.0 and ipfs-webui v2.11.2 (#1638)
    Co-authored-by: Marcin Rataj <lidel@lidel.org>

--- src/common/consts.js
+++ src/common/consts.js
@@ -5,10 +5,8 @@ module.exports = Object.freeze({
   IS_MAC: os.platform() === 'darwin',
   IS_WIN: os.platform() === 'win32',
   VERSION: packageJson.version,
-  GO_IPFS_VERSION: packageJson['go-ipfs']
-    ? packageJson['go-ipfs'].version.slice(1)
-    : packageJson.dependencies['go-ipfs-dep'],
   ELECTRON_VERSION: packageJson.dependencies.electron,
+  GO_IPFS_VERSION: packageJson.dependencies['go-ipfs'],
   COUNTLY_KEY: process.env.NODE_ENV === 'development'
     ? '6b00e04fa5370b1ce361d2f24a09c74254eee382'
     : '47fbb3db3426d2ae32b3b65fe40c564063d8b55d'
