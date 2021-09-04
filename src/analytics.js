c6dd22be9150172781f616e43929925d03b84c63
milahu <milahu@gmail.com>
2021-09-04 11:25:02 +0200

    fix: store analytics in app.getPath(userData)
    userData The directory for storing your app's configuration files, which
    by default it is the appData directory appended with your app's name.
    https://github.com/electron/electron/blob/main/docs/api/app.md#appgetpathname

--- src/analytics.js
+++ src/analytics.js
@@ -1,13 +1,24 @@
 const Countly = require('countly-sdk-nodejs')
 const { ipcMain } = require('electron')
 const { COUNTLY_KEY } = require('./common/consts')
+const { join } = require('path')
+const { app } = require('electron')
+const { existsSync, mkdirSync } = require('fs')
 
 module.exports = async function (ctx) {
+  // workaround: recursive mkdir https://github.com/Countly/countly-sdk-nodejs/pull/14
+  const countlyDataDir = join(app.getPath('userData'), 'countly-data')
+  if (!existsSync(countlyDataDir)) {
+    mkdirSync(countlyDataDir, { recursive: true })
+  }
+
   Countly.init({
     url: 'https://countly.ipfs.io',
     app_key: COUNTLY_KEY,
     debug: process.env.DEBUG_COUNTLY === 'true',
-    require_consent: true
+    require_consent: true,
+    // countlyDataDir for read-only node_modules
+    storage_path: countlyDataDir
   })
 
   ctx.countlyDeviceId = Countly.device_id
