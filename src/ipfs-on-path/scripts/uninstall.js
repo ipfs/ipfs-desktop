b4a5af2b736c4422ccdfa2ca07770cf72f5b5bcc
Henrique Dias <hacdias@gmail.com>
2022-01-21 14:54:57 +0100

    cleanup uninstall.js file

--- src/ipfs-on-path/scripts/uninstall.js
+++ src/ipfs-on-path/scripts/uninstall.js
@@ -2,18 +2,8 @@ const { argv } = require('yargs')
 const fs = require('fs-extra')
 const { join } = require('path')
 
-function checkErr (err) {
-  // NOTE: there is a bug where copySync will throw if the inode
-  // from the src and dst are equal. In the case of symlinks, that'll happen.
-  // https://github.com/jprichardson/node-fs-extra/issues/657
-  if (!err.toString().includes('Source and destination must not be the same')) {
-    throw err
-  }
-}
-
 function revert (userData, bin, dst) {
   const backup = join(userData, bin + '.bak')
-
 
   try {
     fs.unlinkSync(backup)
