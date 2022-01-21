ipfs/ipfs-desktop

commit a246daa918febc2db9100fc57f07d48ba36fd300

chore: update ipfs deps (#1899)
* chore: update ipfs deps
* chore: ipfs-http-client and electron-notarize
* chore: update ipfs http client and ipfsd-ctl
* remove cors logic
* chore: remove deprecated i18next-node-fs-backend
* check for ECONNREFUSED too

License: MIT
Signed-off-by: Henrique Dias <hacdias@gmail.com>
Co-authored-by: Marcin Rataj <lidel@lidel.org>

src/daemon/daemon.js
@@ -79,7 +79,7 @@ module.exports = async function (opts) {
     logger.info(`[daemon] PeerID is ${id}`)
     logger.info(`[daemon] Repo is at ${ipfsd.path}`)
   } catch (err) {
-    if (!err.message.includes('ECONNREFUSED')) {
+    if (!err.message.includes('ECONNREFUSED') || !err.message.includes('ERR_CONNECTION_REFUSED')) {
       throw err
     }
 

src/common/consts.js
@@ -6,7 +6,7 @@ module.exports = Object.freeze({
   IS_WIN: os.platform() === 'win32',
   IS_APPIMAGE: typeof process.env.APPIMAGE !== 'undefined',
   VERSION: packageJson.version,
-  ELECTRON_VERSION: packageJson.dependencies.electron,
+  ELECTRON_VERSION: packageJson.devDependencies.electron,
   GO_IPFS_VERSION: packageJson.dependencies['go-ipfs'],
   COUNTLY_KEY: process.env.NODE_ENV === 'development'
     ? '6b00e04fa5370b1ce361d2f24a09c74254eee382'

src/daemon/config.js
@@ -1,6 +1,6 @@
 const { join } = require('path')
 const fs = require('fs-extra')
-const multiaddr = require('multiaddr')
+const { multiaddr } = require('multiaddr')
 const http = require('http')
 const portfinder = require('portfinder')
 const { shell } = require('electron')

src/webui/index.js
@@ -1,7 +1,6 @@
 const { screen, BrowserWindow, ipcMain, app, session } = require('electron')
 const { join } = require('path')
 const { URL } = require('url')
-const toUri = require('multiaddr-to-uri')
 const serve = require('electron-serve')
 const os = require('os')
 const openExternal = require('./open-external')
@@ -67,16 +66,6 @@ const createWindow = () => {
   })
 
   return window
-}
-
-// Converts a Multiaddr to a valid value for Origin HTTP header
-const apiOrigin = (apiMultiaddr) => {
-  // Return opaque origin when there is no API yet
-  // https://html.spec.whatwg.org/multipage/origin.html#concept-origin-opaque
-  if (!apiMultiaddr) return 'null'
-  // Return the Origin of HTTP API
-  const apiUri = toUri(apiMultiaddr, { assumeHttp: true })
-  return new URL(apiUri).origin
 }
 
 module.exports = async function (ctx) {
@@ -145,24 +134,10 @@ module.exports = async function (ctx) {
     })
   })
 
-  // Avoid setting CORS by acting like /webui loaded from API port
+  // Set user agent
   session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
-    details.requestHeaders.Origin = apiOrigin(apiAddress)
     details.requestHeaders['User-Agent'] = `ipfs-desktop/${VERSION} (Electron ${ELECTRON_VERSION})`
     callback({ cancel: false, requestHeaders: details.requestHeaders }) // eslint-disable-line
-  })
-
-  // modify CORS preflight on the fly
-  const webuiOrigin = 'webui://-'
-  const acao = 'Access-Control-Allow-Origin'
-  session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
-    const { responseHeaders } = details
-    // If Access-Control-Allow-Origin header is returned, override it to match webuiOrigin
-    if (responseHeaders && responseHeaders[acao]) {
-      responseHeaders[acao] = webuiOrigin
-    }
-    // eslint-disable-next-line
-    callback({ responseHeaders })
   })
 
   return new Promise(resolve => {

commit 3b138478835117469a4dcd89fca59e3fb25e5e18

chore: smaller dep updates (#1959)
* chore: smaller dep updates
* fix: correctly remove $IPFS_PATH/api file

This fixes regression introduced by #1899

src/daemon/daemon.js
@@ -79,7 +79,7 @@ module.exports = async function (opts) {
     logger.info(`[daemon] PeerID is ${id}`)
     logger.info(`[daemon] Repo is at ${ipfsd.path}`)
   } catch (err) {
-    if (!err.message.includes('ECONNREFUSED') || !err.message.includes('ERR_CONNECTION_REFUSED')) {
+    if (!err.message.includes('ECONNREFUSED') && !err.message.includes('ERR_CONNECTION_REFUSED')) {
       throw err
     }
 