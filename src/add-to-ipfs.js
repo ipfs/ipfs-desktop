ae5a4a82040588c1d4e30238d34d5f2426445c0a
Henrique Dias <hacdias@gmail.com>
2022-02-09 18:00:10 +0100

    fix: use new glob source API
    see https://github.com/ipfs/js-ipfs/pull/3889

--- src/add-to-ipfs.js
+++ src/add-to-ipfs.js
@@ -89,7 +89,7 @@ module.exports = async function ({ getIpfsd, launchWebUI }, files) {
 
   await Promise.all(files.map(async file => {
     try {
-      const result = await ipfsd.api.add(globSource(file, { recursive: true }), { pin: false })
+      const result = await ipfsd.api.add(globSource(file, '**/*'), { pin: false })
       await copyFile(ipfsd.api, result.cid, result.path)
       successes.push(result)
     } catch (e) {
