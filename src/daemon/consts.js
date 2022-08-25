6a9a9856692a973484bfe81cd2cb59b400451545
Henrique Dias <hacdias@gmail.com>
2022-08-25 21:23:43 +0100

    fix: re-add Object.freeze to STATUS

--- src/daemon/consts.js
+++ src/daemon/consts.js
@@ -4,13 +4,13 @@
  * @type {Object.<string, number>}
  * @readonly
  */
-const STATUS = {
+const STATUS = Object.freeze({
   STARTING_STARTED: 1,
   STARTING_FINISHED: 2,
   STARTING_FAILED: 3,
   STOPPING_STARTED: 4,
   STOPPING_FINISHED: 5,
   STOPPING_FAILED: 6
-}
+})
 
 module.exports = { STATUS }
