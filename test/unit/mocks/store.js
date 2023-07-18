73517d4fb5a9f031b8a7ef7840a3f932c2569485
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-07-18 16:31:47 -0700

    test: mock store doesn't run migrations

--- test/unit/mocks/store.js
+++ test/unit/mocks/store.js
@@ -1,11 +1,16 @@
 const mockElectron = require('./electron')
 const mockLogger = require('./logger')
+const Store = require('electron-store')
 const proxyquire = require('proxyquire').noCallThru()
 
 module.exports = function mockStore () {
+  function MockElectronStoreConstructor ({ ...options }) {
+    return new Store({ ...options, migrations: undefined })
+  }
   // use the real store object, but mock the logger and electron
   return proxyquire('../../../src/common/store', {
     electron: mockElectron(),
-    './logger': mockLogger()
+    './logger': mockLogger(),
+    'electron-store': MockElectronStoreConstructor
   })
 }
