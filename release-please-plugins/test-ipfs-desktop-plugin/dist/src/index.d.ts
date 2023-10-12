0c77858c0850d3f39510a43e5365beb2702372b0
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-10-12 15:38:38 -0700

    chore: remove release-please-plugin from this codebase

--- release-please-plugins/test-ipfs-desktop-plugin/dist/src/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function init(...args: unknown[]): void;
-//# sourceMappingURL=index.d.ts.map
--- release-please-plugins/test-ipfs-desktop-plugin/dist/src/readme-updater.d.ts
+++
@@ -1,8 +0,0 @@
-import { DefaultUpdater, type UpdateOptions } from 'release-please/build/src/updaters/default';
-import { type Logger } from 'release-please/build/src/util/logger';
-export declare class UpdateVersionsInReadme extends DefaultUpdater {
-    constructor(options: UpdateOptions);
-    getRegex(oldVersion: string): RegExp;
-    updateContent(content: string, logger?: Logger): string;
-}
-//# sourceMappingURL=readme-updater.d.ts.map
